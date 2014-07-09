ac020d4b7775dabcda3405b6084d91c67e68d6ab
Seb Tremblay <sebastien@empireone.com.au>
2014-07-10 09:42:40 +1000

    Initial std rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NinefoldsCron::Application.config.secret_key_base = 'e32d0a0d715cf33f5d39f219b987378b58fad9b8282df4f1a6b84e2c44da3153122e7c2804eccf845fb751547f451c479a714740feb43d575f8e2a428fc27f62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NinefoldsCron::Application.config.session_store :cookie_store, key: '_ninefolds-cron_session'
