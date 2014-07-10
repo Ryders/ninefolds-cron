36c846496bfaaa6058b65de2b4166ffaa67de286
Seb Tremblay <sebastien@empireone.com.au>
2014-07-10 19:19:41 +1000

    Testing whether we can run on a single node

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,8 +8,16 @@ every 1.minute do
   rake 'cron:hi'
 end
 
-every :day, :roles => [:web1,:web2] do
+every 1.minute, :roles => [:web1,:web2] do
   rake 'cron:web'
+end
+
+every 1.minute, :roles => [:web1] do
+  command "echo Running WEB shit on `hostname` ONLY"
+end
+
+every 1.minute, :roles => [:worker1] do
+  command "echo Running WORKER shit on `hostname` ONLY"
 end
 
 every 1.minute, :roles => [:worker1,:worker2] do
